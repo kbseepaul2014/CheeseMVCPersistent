d47e60f8af1cf861d3720653638cc9603bd4c743
Kevin Seepaul <kbseepaul@gmail.com>
2019-12-10 22:27:03 -0500

    Cheese

---
+++ src/CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public int CategoryID { get; set; }
+
+        // Replace CheeseType with CheeseCategory
+        public CheeseCategory Category { get; set; }
+
+        public List<CheeseMenu> CheeseMenus { get; set; }
+        
+    }
+}
